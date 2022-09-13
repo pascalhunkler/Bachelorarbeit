fec63e163d889be0e651650af5b71cb758961cca
Pascal Hunkler <pascal.hunkler@neptun.uni-freiburg.de>
2022-09-13 04:23:14 +0200

    Durchschnittsdichte Ausgabe

---
+++ src/compute_average_density_error.py
@@ -0,0 +1,15 @@
+import sys
+
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+    in_file = sys.argv[1]
+    df = pd.read_csv(in_file, sep="\t")
+    average_density = df["Durchschnittsdichte"]
+    mean = average_density.mean()
+    mad = average_density.mad()
+    print("Mittelwert: " + str(mean))
+    print("Durchschnittsdichtefehler " + str(mad))
+    #average_density = np.array(df["Durchschnittsdichte"])
+    #average_density.
