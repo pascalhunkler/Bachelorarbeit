79343673e7ce4ada0c67e4454d38f852f8bf25fe
Pascal Hunkler <pascal.hunkler@neptun.uni-freiburg.de>
2022-09-18 05:53:38 +0200

    Einige Performance Ergebnisse in eine Tabelle

--- src/compute_average_iterations.py
+++ src/compute_average_iterations.py
@@ -6,9 +6,7 @@ import pandas as pd
 if __name__ == "__main__":
     in_file = sys.argv[1]
     df = pd.read_csv(in_file, sep="\t")
-    average_density = df["Durchschnittsdichte"]
+    average_density = df["Iterationen"]
     mean = average_density.mean()
     mad = average_density.mad()
     print("Mittelwert: " + str(mean))
-    #average_density = np.array(df["Durchschnittsdichte"])
-    #average_density.
